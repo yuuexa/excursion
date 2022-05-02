dcbd8522743c7bb01edb1ecc62c95ab0c67646ab
yuuexa <91273572+yuuexa@users.noreply.github.com>
2022-05-02 10:12:01 +0900

    Update loading.js

--- docs/codes/loading.js
+++ docs/codes/loading.js
@@ -1,7 +1,7 @@
 //テキストのカウントアップ+バーの設定
 var bar = new ProgressBar.Line(splash_text, {//id名を指定
     easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-    duration: 5000,//時間指定(1000＝1秒)
+    duration: 3000,//時間指定(1000＝1秒)
     strokeWidth: 0.25,//進捗ゲージの太さ
     color: '#89c3eb',//進捗ゲージのカラー
     trailWidth: 0.25,//ゲージベースの線の太さ
