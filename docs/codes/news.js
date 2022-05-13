fc5306b7fbd81703bcc6e2554b5e781571ad8b42
yuuexa <91273572+yuuexa@users.noreply.github.com>
2022-05-14 02:00:34 +0900

    Add files via upload

--- docs/codes/news.js
+++ docs/codes/news.js
@@ -1,23 +1,15 @@
 var slider;
-var sliderFlag = false;
-var breakpoint = 768;//768px以下の場合
 function sliderSet() {
-        var windowWidth = window.innerWidth;
-        if (windowWidth >= breakpoint && !sliderFlag) {//768px以上は1行でスライダー表示
-            slider = $('.news').bxSlider({
-            touchEnabled:false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
-      mode: 'vertical',//縦スライド指定
-      controls: false,//前後のコントロールを表示させない。
-      auto: 'true',//自動的にスライド
-      pager: false//ページ送り無効化
+    slider = $('.news').bxSlider({
+        touchEnabled:false,
+        mode: 'vertical',
+        controls: false,
+        auto: 'true',
+        pager: false,
+        loop: true,
     });
-            sliderFlag = true;
-        } else if (windowWidth < breakpoint && sliderFlag) {
-            slider.destroySlider();//bxSliderのOptionであるdestroySliderを使用してスライダーの動きを除去
-            sliderFlag = false;
-        }
-    }
+};
 
 $(window).on('load resize', function() {
-        sliderSet();
+    sliderSet();
 });
